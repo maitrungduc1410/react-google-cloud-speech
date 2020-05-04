8a5fc9ab0132f48455f004d2513e034af83c17f7
Mai Trung Duc <maitrungduc1410@gmail.com>
2020-05-04 16:32:03 +0800

    first commit

--- README.md
+++ README.md
@@ -1,68 +1,31 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+<h1 align="center">
+  <div>
+    <img src="./demo.gif" />
+  </div>
+</h1>
 
-## Available Scripts
+# Brief
+This is a simple demo how to use Google Cloud Speech with ReactJS and NodeJS
 
-In the project directory, you can run:
+# Usage
+First install dependencies by running:
+```
+npm install
+```
 
-### `yarn start`
+### Start client
+Run `npm start` to start VueJS client
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+### Start backend
+Before start backend you have to export an environment that links to your Google Service Account Key
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
+Eg:
+```
+export GOOGLE_APPLICATION_CREDENTIALS="absolute_path_to_service_acount_key_file"
+```
+Then in the same terminal, run the following command to start backend:
+```
+npm run dev
+```
 
-### `yarn test`
-
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `yarn build`
-
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `yarn build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+Now you're ready to go
--- src/App.js
+++ src/App.js
@@ -1,26 +1,134 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from 'react'
+import './App.css'
+import io from 'socket.io-client'
+import logo from './logo.svg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let bufferSize = 2048,
+	context,
+	processor,
+	input,
+	globalStream
+
+const downsampleBuffer = (buffer, sampleRate, outSampleRate) => {
+	if (outSampleRate === sampleRate) {
+		return buffer
+	}
+	if (outSampleRate > sampleRate) {
+		throw new Error('downsampling rate show be smaller than original sample rate')
+	}
+	var sampleRateRatio = sampleRate / outSampleRate
+	var newLength = Math.round(buffer.length / sampleRateRatio)
+	var result = new Int16Array(newLength)
+	var offsetResult = 0
+	var offsetBuffer = 0
+	while (offsetResult < result.length) {
+		var nextOffsetBuffer = Math.round((offsetResult + 1) * sampleRateRatio)
+		var accum = 0, count = 0
+		for (var i = offsetBuffer; i < nextOffsetBuffer && i < buffer.length; i++) {
+			accum += buffer[i]
+			count++
+		}
+
+		result[offsetResult] = Math.min(1, accum / count) * 0x7FFF
+		offsetResult++
+		offsetBuffer = nextOffsetBuffer
+	}
+	return result.buffer
 }
 
-export default App;
+
+
+function App() {
+  const socket = useRef(null)
+  const [transcript, setTranscript] = useState('')
+  const [isStreaming, setIsStreaming] = useState(false)
+
+  useEffect(() => {
+    setupSocket()
+  }, [])
+
+  const setupSocket = () => {
+    socket.current = io('http://localhost:3001', {
+      reconnection: true,
+      reconnectionDelay: 1000,
+      reconnectionDelayMax: 5000,
+      reconnectionAttempts: Infinity,
+    })
+
+    socket.current.on('connect', () => {
+      console.log('connected')
+    })
+
+    socket.current.on('data', (data) => {
+      setTranscript(data)
+    })
+  }
+
+  const setup = async () => {
+    context = new (window.AudioContext || window.webkitAudioContext)({
+      // if Non-interactive, use 'playback' or 'balanced' // https://developer.mozilla.org/en-US/docs/Web/API/AudioContextLatencyCategory
+      latencyHint: 'interactive',
+    })
+    processor = context.createScriptProcessor(bufferSize, 1, 1)
+    processor.connect(context.destination)
+    context.resume()
+
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true })
+    globalStream = stream
+    input = context.createMediaStreamSource(stream)
+    input.connect(processor)
+
+    processor.onaudioprocess = (e) => {
+      microphoneProcess(e)
+    }
+  }
+
+  const microphoneProcess = (e) => {
+    var left = e.inputBuffer.getChannelData(0)
+    var left16 = downsampleBuffer(left, 44100, 16000)
+    socket.current.emit('stream', left16)
+  }
+
+  const start = () => {
+    setup()
+    setIsStreaming(true)
+  }
+
+  const stop = () => {
+    if (isStreaming) {
+      // stop record track from browser
+      let track = globalStream.getTracks()[0]
+      track.stop()
+
+      // stop recorder from browser
+      input.disconnect(processor)
+      processor.disconnect(context.destination)
+      context.close().then(() => {
+        input = null
+        processor = null
+        context = null
+        socket.current.emit('stop')
+      })
+
+      setIsStreaming(false)
+    }
+  }
+  
+  return (
+    <div className="App">
+      <div>
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
+      <h1>React-Node-Google Cloud Speech Demo</h1>
+      <div>
+        <button onClick={start}>Record</button>
+        <button onClick={stop}>Stop</button>
+      </div>
+      <div>
+        <textarea value={transcript} readOnly={true} rows="10"></textarea>
+      </div>
+    </div>
+  )
+}
+
+export default App
